e7d19e1f04e853a815a55744cbd93a6326c7dcd2
Andrew <https://github.com/AndreyTantsiura>
2021-08-16 15:34:34 +0300

    add side bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from "./logo.svg";
+
 import "./App.css";
+import { Switch, Route, NavLink } from "react-router-dom";
+import logo from "./img/logo-icon.png";
+
+function DashboardComponent() {
+  return <div></div>;
+}
+
+function ActivityComponent() {
+  return <div></div>;
+}
+
+function LibraryComponent() {
+  return <div></div>;
+}
+
+function SettingsComponent() {
+  return <div></div>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="side_bar">
+        <div className="logo">
+          <img src={logo} alt="" />
+          <div className="text_logo">
+            <div className="text_logo_money">MONEY</div>
+            <span>tracker</span>
+          </div>
+        </div>
+        <div className="side_bar_inner">
+          <div className="side_bar_inner_top">
+            <NavLink exact to="/">
+              <div className="frame">&#128471; Dashboard</div>
+            </NavLink>
+            <NavLink exact to="/activity">
+              <div className="frame">&#128498; Activity</div>
+            </NavLink>
+            <NavLink exact to="/library">
+              <div className="frame">&#128366; Library</div>
+            </NavLink>
+          </div>
+          <NavLink exact to="/settings">
+            <div className="frame">&#9881; Settings</div>
+          </NavLink>
+        </div>
+      </div>
+      <Switch>
+        <Route exact path="/" component={DashboardComponent}></Route>
+        <Route path="/activity" component={ActivityComponent}></Route>
+        <Route path="/library" component={LibraryComponent}></Route>
+        <Route path="/settings" component={SettingsComponent}></Route>
+      </Switch>
     </div>
   );
 }
