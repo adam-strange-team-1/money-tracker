700469ec8b4a32ea4af46ab8179d7b0ff11de676
Olha Fastiuk <olhafastiuk@gmail.com>
2021-08-29 18:18:01 +0300

    Update styled

--- src/components_activity/Activity.jsx
+++ src/components_activity/Activity.jsx
@@ -1,17 +1,27 @@
 import React from "react";
-import { Flex, InfoBlocks } from "./ActivityStyled";
+import { InfoBlocks } from "./ActivityStyled";
 import BarChart from "./BarChart";
 import PieChart from "./Radial";
+import { Flex, Text, TitleStyled } from "../components/common/StyledComponents";
 
 export default function Activity() {
   return (
-    <Flex>
-      <InfoBlocks width="456px" height="350px" padding="10px 10px 20px">
-        <PieChart />
-      </InfoBlocks>
-      <InfoBlocks width="270px" height="345px" padding="10px 10px 20px">
-        <BarChart />
-      </InfoBlocks>
+    <Flex
+      direction="column"
+      margin="60px 0 0 20vw"
+      width="55vw"
+      align="start"
+      height="100%"
+    >
+      <TitleStyled>Activity</TitleStyled>
+      <Flex margin="0 0 0 0" direction="row" justify="space-between">
+        <InfoBlocks width="456px" height="350px" padding="10px 10px 20px">
+          <PieChart />
+        </InfoBlocks>
+        <InfoBlocks width="270px" height="345px" padding="10px 10px 20px">
+          <BarChart />
+        </InfoBlocks>
+      </Flex>
     </Flex>
   );
 }
